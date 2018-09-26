959dd39b75064b8f2952ed0fc25f8341f32e1b8d
LealTX <jacob.leal32@gmail.com>
2018-09-25 23:34:00 -0500

    html added with Materialize styling

---
+++ assets/javascript/train.js
@@ -0,0 +1,14 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCuOSrsFE_srwYwH4PLturATc6788e5OAU",
+    authDomain: "train-schedule-271a6.firebaseapp.com",
+    databaseURL: "https://train-schedule-271a6.firebaseio.com",
+    projectId: "train-schedule-271a6",
+    storageBucket: "train-schedule-271a6.appspot.com",
+    messagingSenderId: "282138469699"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
